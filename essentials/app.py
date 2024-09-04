364a9edf6e25a968d3139b59af049e9b3cc65b13
abhishek <abhisheksara21@gmail.com>
2024-09-04 15:29:17 +0530

    created new workflow

--- essentials/app.py
+++ essentials/app.py
@@ -1,7 +1,11 @@
+'''
+This is a basic flask application.
+'''
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    '''Returns hello World!'''
     return "<p>Hello, World!</p>"
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,2 +1,6 @@
+'''
+This is a test file.
+'''
 def test_always_pass():
+    '''This test always pass'''
     assert True
